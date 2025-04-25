f5807fcec80e1b05d944e1d6bea5efd7e8678422
moorl <info@moorleiche.com>
2025-04-25 14:47:35 +0200

    sw to mt

--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -1,1 +1,42 @@
-import './module/sw-cms';
+setTimeout(() => {
+    const cmsElementConfig = MoorlFoundation.CmsElementHelper.getCmsElementConfig({
+        name: 'moorl-brand-slider-v2',
+        parent: 'listing',
+        plugin: 'MoorlCmsBrandSlider',
+        icon: 'regular-view-grid',
+        cmsElementEntity: {
+            entity: 'product_manufacturer'
+        },
+        cmsElementMapping: {
+            listingSource: {
+                value: 'static',
+                attributes: {
+                    filter: ['static', 'select'],
+                }
+            },
+            listingLayout: {
+                value: 'slider'
+            },
+            itemLayout: {
+                value: 'image-or-title'
+            },
+            gapSize: {
+                value: '15px'
+            },
+            itemWidth: {
+                value: '220px'
+            },
+            displayMode: {
+                value: 'contain'
+            },
+            itemHeight: {
+                value: '130px'
+            },
+            hasButton: {
+                value: false
+            },
+        }
+    });
+
+    Shopware.Application.getContainer('service').cmsService.registerCmsElement(cmsElementConfig);
+}, 100);
--- src/Resources/app/administration/src/module/sw-cms/elements/index.js
+++
@@ -1,1 +0,0 @@
-import './brand-slider-v2';
