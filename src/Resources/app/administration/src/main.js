2383f1195b584e00685d41f124ecc1d11371d01c
moorl <info@moorleiche.com>
2025-05-02 18:02:06 +0200

    sw to mt

--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -1,42 +1,52 @@
-setTimeout(() => {
-    MoorlFoundation.CmsElementHelper.registerCmsElement({
-        name: 'moorl-brand-slider-v2',
-        parent: 'listing',
-        plugin: 'MoorlCmsBrandSlider',
-        icon: 'regular-view-grid',
-        cmsElementEntity: {
-            entity: 'product_manufacturer'
-        },
-        cmsElementMapping: {
-            listingSource: {
-                value: 'static',
-                attributes: {
-                    filter: ['static', 'select'],
+function onMoorlFoundationReady(callback) {
+    if (window.MoorlFoundation) {
+        callback();
+    } else {
+        window.addEventListener('MoorlFoundationReady', callback, { once: true });
+    }
+}
+
+onMoorlFoundationReady(() => {
+    MoorlFoundation.ModuleHelper.registerModule({
+        cmsElements: [
+            {
+                name: 'moorl-brand-slider-v2',
+                parent: 'listing',
+                plugin: 'MoorlCmsBrandSlider',
+                icon: 'regular-view-grid',
+                cmsElementEntity: {
+                    entity: 'product_manufacturer'
+                },
+                cmsElementMapping: {
+                    listingSource: {
+                        value: 'static',
+                        attributes: {
+                            filter: ['static', 'select'],
+                        }
+                    },
+                    listingLayout: {
+                        value: 'slider'
+                    },
+                    itemLayout: {
+                        value: 'image-or-title'
+                    },
+                    gapSize: {
+                        value: '15px'
+                    },
+                    itemWidth: {
+                        value: '220px'
+                    },
+                    displayMode: {
+                        value: 'contain'
+                    },
+                    itemHeight: {
+                        value: '130px'
+                    },
+                    hasButton: {
+                        value: false
+                    },
                 }
-            },
-            listingLayout: {
-                value: 'slider'
-            },
-            itemLayout: {
-                value: 'image-or-title'
-            },
-            gapSize: {
-                value: '15px'
-            },
-            itemWidth: {
-                value: '220px'
-            },
-            displayMode: {
-                value: 'contain'
-            },
-            itemHeight: {
-                value: '130px'
-            },
-            hasButton: {
-                value: false
-            },
-        }
+            }
+        ]
     });
-
-
-}, 100);
+});
